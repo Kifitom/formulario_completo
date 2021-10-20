28f8120626652a063ea73f17f5f90c4e48a92458
Kifitom <kifitom@gmail.com>
2021-10-20 17:18:40 -0500

    Pagina aun no terminada, falta efectuar el js

---
+++ js_formulario_completo/boton.js
@@ -0,0 +1,32 @@
+alert ("🛑 ¡¡Pagina en modo beta!! 🛑 ")
+
+
+
+
+
+function llevardatos () {
+    let fav = document.getElementById("fav").value
+    let cantidad = document.getElementById("cantidad").value
+    let Pregunta_1 = document.getElementById("Pregunta_1").value
+    let anime1 = document.getElementById("anime1").value
+    let anime2 = document.getElementById("anime2").value
+    let anime3 = document.getElementById("anime3").value
+    let anime4 = document.getElementById("anime4").value
+    let anime5 = document.getElementById("anime5").value
+    let anime6 = document.getElementById("anime6").value
+    let anime7 = document.getElementById("anime7").value
+
+    if (cantidad <=15) {
+        alert ("has visto pocos")
+    } else {
+         alert ("wuau")
+    }
+
+
+
+
+
+
+
+
+} 
